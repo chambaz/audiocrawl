9af1b63ca36d024119a8c29e9ea8476cb21ab178
Adam Chambers <adammchambers@gmail.com>
2019-01-25 17:04:13 -0500

    🚧  Include modal as template and hook up JS to instantiate modal from template if none exists

--- components/modal/index.js
+++ components/modal/index.js
@@ -1,25 +1,60 @@
 import $ from 'jquery'
+import createHistory from 'history/createBrowserHistory'
+
+const history = createHistory()
 
 $(function() {
   const ui = {
     cards: $('[data-card]'),
-    modal: $('[data-modal]'),
-    container: $('[data-modal-container]'),
     close: $('[data-modal-close]')
   }
   let isActive = false
+  let crawlData = {}
 
-  if (ui.modal.hasClass('is-active')) {
+  if ($('[data-modal]').hasClass('is-active')) {
     isActive = true
+  } else {
+    const modalTmpl = $('[data-modal-template]')[0]
+    $('body').append($(document.importNode(modalTmpl.content, true)))
   }
 
-  ui.cards.on('click', e => {
+  ui.modal = $('[data-modal]')
+  ui.container = ui.modal.find('[data-modal-container]')
+
+  history.listen((location, action) => {
+    // location is an object like window.location
+    console.log(action, location.pathname, location.state)
+  })
+
+  ui.cards.on('click', function(e) {
     const target = $(e.target)
+
+    e.preventDefault()
+
+    crawlData = JSON.parse($(this).attr('data-card'))
 
     if (target.attr('data-link') || target.parents('[data-link]').length) {
       return
     }
 
+    openModal(crawlData)
+  })
+
+  ui.close.on('click', closeModal)
+
+  ui.modal.on('click', e => {
+    if ($(e.target).is('[data-modal]')) {
+      closeModal()
+    }
+  })
+
+  $('body').on('keyup', e => {
+    if (e.keyCode === 27) {
+      closeModal()
+    }
+  })
+
+  function openModal(data) {
     ui.modal
       .css('display', 'flex')
       .delay(10)
@@ -40,22 +75,9 @@ $(function() {
         100
       )
 
+    history.push(`/crawl/${crawlData.slug}`, data)
     isActive = true
-  })
-
-  ui.close.on('click', closeModal)
-
-  ui.modal.on('click', e => {
-    if ($(e.target).is('[data-modal]')) {
-      closeModal()
-    }
-  })
-
-  $('body').on('keyup', e => {
-    if (e.keyCode === 27) {
-      closeModal()
-    }
-  })
+  }
 
   function closeModal() {
     if (!isActive) {
@@ -68,5 +90,6 @@ $(function() {
     })
 
     isActive = false
+    history.goBack()
   }
 })
