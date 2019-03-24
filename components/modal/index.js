f8b7875de87ab41eb2843bead594aee1b5bf9d68
Adam Chambers <adammchambers@gmail.com>
2019-03-24 18:31:56 -0400

    ✨  Update document title when crawl modal shown client side

--- components/modal/index.js
+++ components/modal/index.js
@@ -11,6 +11,8 @@ $(function() {
   let isActive = false
   let ctrl = false
 
+  let docTitle = document.title
+
   // if data-modal already exists then we must be on a single entry page
   // e.g /crawl/slug
   // set active to true so JS can take over
@@ -132,6 +134,8 @@ $(function() {
       tagsEl.parent().hide()
     }
 
+    document.title = `${data.title} | Audiocrawl`
+
     openModal()
   }
 
@@ -178,6 +182,7 @@ $(function() {
     }, 150)
 
     isActive = false
+    document.title = docTitle
     history.goBack()
   }
 })
